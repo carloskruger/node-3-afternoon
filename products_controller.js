169026466c445a84f0bef9864e9d12399dfd7839
Carlos Kruger <carloskr@yahoo.com>
2020-11-04 18:33:12 -0500

    node-3-afternoon

---
+++ products_controller.js
@@ -0,0 +1,61 @@
+module.exports = {
+
+    create: (req,res ) => {
+        const dbInstance = req.app.get('db');
+        const { name, description, price, image_url } = req.body;
+        dbInstance.create_product( name, description, price, image_url)
+        .then(() => res.sendStatus(200))
+        .catch(err => {res.status(500).send({errorMessage: "Could not create the product"});
+      console.log(err)
+    });
+    },
+
+
+    getOne: (req,res ) => {
+        const dbInstance = req.app.get('db');
+        const { id } = req.params;
+        dbInstance.read_product( id )
+        .then(product => res.status(200).send( product ))
+        .catch(err => {res.status(500).send({errorMessage: "not found"});
+        console.log(err)
+    })
+    },
+
+
+    getAll: (req,res ) => {
+        const dbInstance = req.app.get('db');
+        dbInstance.read_products()
+        .then(products => res.status(200).send( products ))
+        .catch(err => {res.status(500).send({errorMessage: "server problems"});
+        console.log(err)
+    })
+    },
+
+    update: (req,res ) => {
+        const dbInstance = req.app.get('db');
+        const { id } = req.params
+        const { desc } = req.query
+
+
+    dbInstance.update_product([id, desc])
+    .then( () => res.sendStatus(200) )
+    .catch( err => {
+      res.status(500).send({errorMessage: "Oops! Something went wrong. Our engineers have been informed!"});
+      console.log(err)
+    } );
+
+    },
+    delete: (req,res ) => {
+        const dbInstance = req.app.get('db');
+
+        const { id } = req.params;
+    dbInstance.delete_product(id)
+    .then( () => res.sendStatus(200) )
+    .catch( err => {
+      res.status(500).send({errorMessage: "couldn't delete the product!"});
+      console.log(err)
+    } );
+
+    }
+
+}
